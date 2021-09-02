0fa7fb6324ee7591e93f2947f9f01727621463a0
Md. Shahadat Hossen <mhossen171243@bscse.uiu.ac.bd>
2021-09-02 12:04:47 +0600

    index file

---
+++ first app/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+app = express;
+
+app.get("/", function (req, res) {
+    res.send("hello Express JS");
+});
+
+app.listen(8000, function () {
+    console.log("server run successfully");
+})
