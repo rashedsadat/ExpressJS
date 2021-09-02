8d82928e2d0bf3ce1ec95c5e6801f6912487632c
Md. Shahadat Hossen <mhossen171243@bscse.uiu.ac.bd>
2021-09-02 12:53:41 +0600

    updated index.js

--- first app/index.js
+++ first app/index.js
@@ -3,7 +3,19 @@ var express = require('express');
 app = express();
 
 app.get("/", function (req, res) {
-    res.send("hello Express JS");
+    res.send("This is HOME page");
+});
+
+app.post("/about", function (req, res) {
+    res.send("This is ABOUT page");
+});
+
+app.put("/contact", function (req, res) {
+    res.send("This is CONTACT page");
+});
+
+app.delete("/term", function (req, res) {
+    res.send("This is TERM pageS");
 });
 
 app.listen(8000, function () {
